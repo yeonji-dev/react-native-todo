10bd4a8a0017efddb5ca1ea664d704807368e413
YeonJiNoh <65704179+yeonji-dev@users.noreply.github.com>
2023-09-24 22:02:16 +0900

    Config: initial

---
+++ App.js
@@ -0,0 +1,14 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import App from './src/App'
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
