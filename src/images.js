4546564e574c196a2b8443716cef990d7e26b9c6
YeonJiNoh <65704179+yeonji-dev@users.noreply.github.com>
2023-09-28 13:47:12 +0900

    Feat: images 추가

---
+++ src/images.js
@@ -0,0 +1,11 @@
+import CheckBoxOutline from '../assets/icons/check_box_outline_blank_white.png';
+import CheckBox from '../assets/icons/check_box_white.png';
+import DeleteForever from '../assets/icons/delete_white.png';
+import Edit from '../assets/icons/edit_white.png';
+
+export const images = {
+    uncompleted: CheckBoxOutline,
+    completed: CheckBox,
+    delete: DeleteForever,
+    update: Edit,
+}
